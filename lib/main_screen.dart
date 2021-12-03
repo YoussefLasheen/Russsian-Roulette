d3b57664e6dfd26d83d95385cb14856eb600b1e3
Youssef Lasheen <youssef@lasheen.dev>
2021-12-03 21:53:58 +0200

    Remove FireButton and add GestureDetector as a controller.

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:russian_roulette/widgets/fire_button.dart';
-import 'package:russian_roulette/widgets/modify_cylinder/modify_cylinder.dart';
+import 'package:provider/provider.dart';
+
+import 'models/cylinder_model.dart';
+import 'widgets/modify_cylinder/modify_cylinder.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    CylinderModel cylinder = Provider.of<CylinderModel>(context);
+    bool canShoot = false;
     return Material(
-      child: Row(
-        children: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                PageRouteBuilder(
-                    opaque: false,
-                    pageBuilder: (_, __, ___) => const ModifyCylinder()),
-              );
+      child: GestureDetector(
+        onHorizontalDragStart: (_) {
+          canShoot = true;
+        },
+        onHorizontalDragEnd: (_) {
+          canShoot = true;
+        },
+        onHorizontalDragUpdate: (details) {
+          if (details.delta.dx > 2 && canShoot) {
+            cylinder.fireBulletAndShiftPosition();
+            canShoot = false;
+          }
+        },
+        onVerticalDragUpdate: (details) {
+          if (details.delta.dy > 10) {
+            Navigator.push(
+              context,
+              PageRouteBuilder(
+                  opaque: false,
+                  pageBuilder: (_, __, ___) => const ModifyCylinder()),
+            );
             },
             icon: Icon(Icons.settings),
           ),
